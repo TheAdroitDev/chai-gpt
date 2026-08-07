61441d8ee404db7be642b858badf2351283bbcc2
GitPirates <starshivamverma2@gmail.com>
2026-08-07 16:30:14 +0530

    feat: implement theme provider and mode toggle components; update layout and page structure

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,8 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono, Inter } from "next/font/google";
 import "./globals.css";
 import { cn } from "@/lib/utils";
+import { ThemeProvider } from "@/components/ui/providers/theme-provider";
+
 
 const inter = Inter({subsets:['latin'],variable:'--font-sans'});
 
@@ -23,10 +25,20 @@ export const metadata: Metadata = {
 export default function RootLayout({ children }: LayoutProps<"/">) {
   return (
     <html
-      lang="en"
+      lang="en" suppressHydrationWarning
       className={cn("h-full", "antialiased", geistSans.variable, geistMono.variable, "font-sans", inter.variable)}
     >
-      <body className="min-h-full flex flex-col">{children}</body>
+      
+      <body className="min-h-full flex flex-col">
+        {children}
+         <ThemeProvider
+            attribute="class"
+            defaultTheme="system"
+            enableSystem
+            disableTransitionOnChange
+          ></ThemeProvider></body>
+
+      
     </html>
   );
 }
