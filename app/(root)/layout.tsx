5730307d0703a389adec2b5d0fb4f13fb31e3f48
GitPirates <starshivamverma2@gmail.com>
2026-08-08 22:45:10 +0530

    feat: wrap children in ChatShell component in layout

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,6 +1,7 @@
 import { auth } from '@clerk/nextjs/server'
 import React from 'react'
 import { onBoard } from '../features/auth/action/onboard';
+import { ChatShell } from '../features/conversation/components/chat-shell';
 
 
 // Authenticated app layout — protects routes, syncs user to DB, and wraps content in `ChatShell` later !.
@@ -12,7 +13,9 @@ const RootGrouplayout = async ({ children }: { children: React.ReactNode }) => {
 
     return (
         <>
+        <ChatShell>
         { children }
+        </ChatShell>
         </>
     )
 }
